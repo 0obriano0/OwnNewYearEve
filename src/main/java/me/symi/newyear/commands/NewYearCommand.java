d389d496833ec1a7217294d88153933e1048b009
0obriano0 <37872600+0obriano0@users.noreply.github.com>
2021-03-07 17:37:39 +0800

    Update NewYearCommand.java

--- src/main/java/me/symi/newyear/commands/NewYearCommand.java
+++ src/main/java/me/symi/newyear/commands/NewYearCommand.java
@@ -14,8 +14,6 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.Random;
-
 public class NewYearCommand implements CommandExecutor {
 
     private final OwnNewYearEve plugin;
